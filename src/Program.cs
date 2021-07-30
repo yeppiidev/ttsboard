971fd4a092a173ab528cbd69b366efbfbd7dbc0b
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2021-07-30 20:18:19 +0530

    Theme now change according to your OS

--- src/Program.cs
+++ src/Program.cs
@@ -9,13 +9,42 @@ namespace TTS
 {
     class Program
     {
+        static Boolean ShouldEnableVisualStyles() {
+            //Get Operating system information.
+            OperatingSystem os = Environment.OSVersion;
+            //Get version information about the os.
+            Version vs = os.Version;
+
+            if (os.Platform == PlatformID.Win32Windows || os.Platform == PlatformID.Win32NT)
+            {
+                //This is a pre-NT version of Windows
+                switch (vs.Major)
+                {
+                    case 5:
+                    case 4:
+                    case 90:
+                        return false;
+                    case 10:
+                    case 6:
+                        return true;
+                    default:
+                        return true;
+                        break;
+                }
+            } else
+            {
+                return false;
+            }
+        }
+
         [STAThread]
         static void Main(string[] args)
         {
             MainForm mainForm = new MainForm();
 
+            if (ShouldEnableVisualStyles())
+                Application.EnableVisualStyles();
             Application.Run(mainForm);
-            Application.EnableVisualStyles();
         }
     }
 }
